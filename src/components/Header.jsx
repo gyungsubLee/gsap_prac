e206fd8941d4db4fcfc2866fb25ed9a2d5d3bf43
gyoungsubLee <dltjq2323@gmail.com>
2025-04-13 14:04:20 +0900

    fix: 이미지 불러오기 오류 수정

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto">
+        테스트
+        <a href="/" className="text-2xl font-bold">
+          <span>
+            <img src="images/logo/L_G.png" alt=""></img>
+          </span>
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
